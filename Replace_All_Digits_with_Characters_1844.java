4c4b997f2733bb6cf817e5a7ec422d983ab1665d
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-01-18 23:52:12 +0530

    1844_Replace_All_Digits_with_Characters

---
+++ Replace_All_Digits_with_Characters_1844.java
@@ -0,0 +1,10 @@
+public class Replace_All_Digits_with_Characters_1844 {
+    public String replaceDigits(String s) {
+
+        char[] A = s.toCharArray();
+
+        for (int i = 1; i < A.length; i += 2)
+            A[i] += (char) (A[i - 1] - '0');
+
+        return String.valueOf(A);
+    }
